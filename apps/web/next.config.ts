79d3862fca17f4eb8e0c0694782c70bfa4e3e46a
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-12-10 21:49:55 +0530

    Cloud Flare deploy Error Fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // to fix the Cloudflare Deploy Erros
+   transpilePackages: ['@repo/ui'],
 };
 
 export default nextConfig;
